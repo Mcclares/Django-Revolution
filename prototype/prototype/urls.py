167191f0b40a22a60413d119a046a5009e96b9a4
Mcclares <86326552+Mcclares@users.noreply.github.com>
2022-01-10 18:44:19 +0300

    FirstSite

---
+++ prototype/prototype/urls.py
@@ -0,0 +1,10 @@
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls))
+]
